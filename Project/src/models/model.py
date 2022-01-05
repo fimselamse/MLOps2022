7315ab6f20c4d56f0308bc685092212070765143
Hans Christian Milman <hanschrmil@gmail.com>
2022-01-05 09:46:18 +0100

    CNN
    added CNN model

--- Project/src/models/model.py
+++ Project/src/models/model.py
@@ -2,20 +2,54 @@ from torch import nn
 import torch.nn.functional as F
 
 
-class MyAwesomeModel(nn.Module):
+class LinearModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(784, 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64, 10)
-
-        self.dropout = nn.Dropout(p=0.2)
-
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+        
     def forward(self, x):
+        # make sure input tensor is flattened
         x = x.view(x.shape[0], -1)
-
-        x = self.dropout(F.relu(self.fc1(x)))
-        x = self.dropout(F.relu(self.fc2(x)))
-        out = F.log_softmax(self.fc3(x), dim=1)
-
-        return out
+        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.log_softmax(self.fc4(x), dim=1)
+        
+        return x
+    
+    
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=3,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        self.linear = nn.Linear(32*7*7, 10)
+        
+    def forward(self, x):
+        # make sure input tensor is flattened
+        x = x.reshape((x.shape[0], 1, x.shape[1], x.shape[1]), x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        
+        x = x.view(x.shape[0], -1)
+        x = F.log_softmax(self.linear(x), dim=1)
+        
+        return x
