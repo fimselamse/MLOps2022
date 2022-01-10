fcdd636d143b8adaf387c46600cea2bad3ef14d1
Hans Christian Milman <hanschrmil@gmail.com>
2022-01-10 13:01:00 +0100

    Update test_training.py

--- Project/src/tests/test_training.py
+++ Project/src/tests/test_training.py
@@ -9,5 +9,5 @@ def test_training():
     # training network not really set up for this kind of test.
     # since it uses hydra and a config file, most errors will be logged 
     # at runtime
-    assert 1==1
+    assert 2==1
     
